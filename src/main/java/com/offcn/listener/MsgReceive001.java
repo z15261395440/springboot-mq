ce6ef44aed74067e107594aaa81d8f62df6a6fea
zxr001 <984951937@qq.com>
2019-11-16 13:55:52 +0800

    修改myreceive

--- src/main/java/com/offcn/listener/MsgReceive001.java
+++ src/main/java/com/offcn/listener/MsgReceive001.java
@@ -12,7 +12,7 @@ public class MsgReceive001 {
         System.out.println(msg+"接收到消息");
     }
 
-    //接收map
+    //接收map  2019 11 16
     @JmsListener(destination = "mq-test002")
     public void receiveMap(Map map){
         System.out.println("接收到map:"+map.get("name")+":"+map.get("phone")+":"+map.get("address"));
